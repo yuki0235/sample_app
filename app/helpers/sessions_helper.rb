4d4c47d40744281ee76bab608c7a3101c979a78b
nyakanishi <yuto0235@gmail.com>
2019-12-08 08:45:10 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,9 +21,9 @@ module SessionsHelper
     end
 	end
 
-	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
-	end
+	# def current_user
+	# 	@current_user ||= User.find_by(id: session[:user_id])
+	# end
 
 	def logged_in?
 		!current_user.nil?
